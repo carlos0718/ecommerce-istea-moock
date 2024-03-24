94cea253debf612f276f6b878c152300f387d670
c.sipiran07@gmail.com <carlos0718>
2024-03-23 21:48:34 -0300

    using api to rendering cards

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const URL = "https://api.escuelajs.co/api/v1/products";
 
-function getProducts() {
+export function getProducts() {
 	return fetch(URL)
 		.then((response) => response.json())
 		.then((data) => data);
--- src/cards.js
+++ src/cards.js
@@ -1,38 +1,45 @@
+import {getProducts} from "./api.js";
+
 export function renderCards() {
 	const $ = document;
 	const cardContainer = $.querySelector("#card-template");
-	[
+
+	/* 	[
 		{id: 1, name: "Product 1", price: 100, quantity: 1},
-		{id: 2, name: "Product 1", price: 100, quantity: 1},
-		{id: 3, name: "Product 1", price: 100, quantity: 1},
-		{id: 4, name: "Product 1", price: 100, quantity: 1},
-		{id: 5, name: "Product 1", price: 100, quantity: 1},
-		{id: 6, name: "Product 1", price: 100, quantity: 1},
-		{id: 7, name: "Product 1", price: 100, quantity: 1},
-		{id: 8, name: "Product 1", price: 100, quantity: 1},
-		{id: 9, name: "Product 1", price: 100, quantity: 1},
-		{id: 10, name: "Product 1", price: 100, quantity: 1},
-	].forEach((product) => {
-		let card = `<div class="col-4">
+		{id: 2, name: "Product 2", price: 100, quantity: 1},
+		{id: 3, name: "Product 3", price: 100, quantity: 1},
+		{id: 4, name: "Product 3", price: 100, quantity: 1},
+		{id: 5, name: "Product 4", price: 100, quantity: 1},
+		{id: 6, name: "Product 5", price: 100, quantity: 1},
+		{id: 7, name: "Product 6", price: 100, quantity: 1},
+		{id: 8, name: "Product 7", price: 100, quantity: 1},
+		{id: 9, name: "Product 8", price: 100, quantity: 1},
+		{id: 10, name: "Product 9", price: 100, quantity: 1},
+	] */
+	getProducts().then((data) => {
+		data.forEach((product) => {
+			console.log("images:", product.images);
+			let card = `<div class="col-4">
 						<div class="card">
-							<img src="" class="card-img-top" alt="...">
+							<img src="https://imgur.com/ZKGofuB" class="card-img-top" alt="...">
 							<div class="card-body">
 								<h3 class="card-title">Title</h3>
-								<p class="card-text">Text</p>
+								<p class="card-text">${product.title}</p>
 							</div>
 							<div>
 								<button onclick="alert('clicked id ${product.id}')">click me!</button>
 							</div>
 						</div>
 					</div>`;
-		cardContainer.innerHTML += card;
+			cardContainer.innerHTML += card;
+		});
 	});
 }
 
 /**
  * another approach
  */
-/* 
+/*
 const tagImg = document.createElement("img");
 tagImg.src = "...";
 tagImg.alt = "...";
@@ -56,4 +63,5 @@ const tagDiv = document.createElement("div");
 tagDiv.appendChild(tagButton);
 
 const tagCardContainer = document.createElement("div");
-tagCardContainer.classList.add("card"); */
+tagCardContainer.classList.add("card");
+*/
